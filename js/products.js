220a3806e15e7e32ffe89d43ee8c54ce7176cb88
Niyaaz Daniels <niyaazdaniels1@gmail.com>
2023-12-05 21:30:45 +0200

    extra

--- js/products.js
+++ js/products.js
@@ -1,62 +1,46 @@
 let newProducts = [];
+let main = document.querySelector('main')
+let cartItems = JSON.parse(localStorage.getItem('products'))
 
-let mainDisplay = document.querySelector('main')
-let products = JSON.parse(localStorage.getItem('products'));
-
-let produce = function extraProducts(){
-mainDisplay.innerHTML = products.map(function(product,index){
-return `
-<div class="products">
-<div class="header"> 
-<h5>${product.make}</h5>
-    </div>
-    <div class="item">
-        <img src="${product.url}" width= "100px" height= "90px"></img>
-    </div>
-    <div class="content">
-    <p>${product.description}</p>
-    <p>R${product.price}</p>
-    </div>
-    <div class = "button">
-    <p><button class = "adtc" data-add >Add to Cart</button></p>
-    </div>
-    </div>
-    `
+function productsDisplay(){
+    let products = cartItems.map(function(product,index){
+        return `
+        <div class="col-xxl container-card d-flex">
+        <div class="card me-1 card-product ">
+        <img src="${product.url}" class="card-img-top  w-25 img-fluid d-flex align-content-lg-center  alt="${product.make}">
+        <div class="card-body d-block">
+          <h5 class="card-title">${product.make}</h5>
+          <p class="card-text">${product.description}</p>
+          <p class="card-text">R${product.price}</p>
+          <button class="btn btn-success " id = "button-add">Add To Cart</button>
+        </div>
+        </div>
+        </div>
+        `
+    }) 
+    main.innerHTML = products.join('')
 }
-)}
+productsDisplay();
 
-// Search function
-
-document.getElementById('searchInput').addEventListener('input', search);
-document.querySelector('select').addEventListener('change', search);
+document.getElementById('searchInput').addEventListener('input', searchContainer);
+document.querySelector('select').addEventListener('change', searchContainer);
 if(product.length===0){
-    mainDisplay.innerHTML = `
-    <div class="spinner-border" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>`
+    main.innerHTML = `<div class="spinner-border text-danger" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>`
+}else{
+    productsDisplay(product)
 }
-function search(){
+function searchContainer(){
     let searchIt = document.getElementById('searchInput').value.toLowerCase();
     let options = document.querySelector('select').value;
-    let sorted = product.filter(product => {
-        return product.make.toLowerCase().includes(searchIt);
+    let sorted = product.filter(item => {
+        return item.name.toLowerCase().includes(searchIt);
     });
     if (options === 'Low to High') {
         sorted.sort((a, b) => a.price - b.price);
     } else if (options === 'High to Low') {
         sorted.sort((a, b) => b.price - a.price);
     }
+    productsDisplay(sorted);
 }
-
-
-// adding functionality for add to cart button that when clicked sends code to localstorage
-function addToCart(index){
-    newProducts.push(products[index])
-    localStorage.setItem("newProducts",JSON.stringify(newProducts))
-}    
-mainDisplay.addEventListener('click', function(event){  
-    if (event.target.hasAttribute('data-add')){
-        addToCart(event.target.value)
-    }    
-})    
-
